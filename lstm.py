c4d2e9bbce801c3f0b30d5096323ab22addfff27
Pradeesh <pradeesh@gmail.com>
2022-08-03 16:31:54 +1200

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -2,6 +2,7 @@
 # It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
 # For example, here's several helpful packages to load in 
 #https://www.kaggle.com/tanumoynandy/sarcasm-detection-rnn-lstm <-- HOME WORK
+#updated comment because why not
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
